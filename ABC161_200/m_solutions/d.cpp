d84934fe4ea69cc863c9ee39b48995d913c5566d
ncyamane <chabajunsei@gmail.com>
2020-07-27 21:15:44 +0900

    M-Solutions の復習

--- ABC161_200/m_solutions/d.cpp
+++ ABC161_200/m_solutions/d.cpp
@@ -2,19 +2,18 @@
 using namespace std;
 
 int main() {
-  int n;
+  int64_t n;
   cin >> n;
-  vector<int> a(n + 1);
+  vector<int64_t> a(n + 1);
   for (int i = 1; i <= n; i++) {
     cin >> a.at(i);
   }
 
-  int money = 1000;
+  int64_t money = 1000;
   for (int i = 1; i < n; i++) {
     if (a.at(i) < a.at(i + 1)) {
-      int stock = money / a.at(i);
-      money -= stock * a.at(i);
-      money += stock * a.at(i + 1);
+      int64_t stock = money / a.at(i);
+      money += stock * (a.at(i + 1) - a.at(i));
     }
   }
 
